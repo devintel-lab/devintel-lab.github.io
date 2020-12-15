816bf7e36f892f47cf24f759c7b716ac33bf5b87
DianZhi <dianzhi97@gmail.com.com>
2020-12-15 02:08:40 -0500

    Implement the nav toggle for phone on all pages

--- assets/js/page_template.js
+++ assets/js/page_template.js
@@ -15,8 +15,10 @@ $('document').ready(function () {
       $(".navbar").css("padding", "0px");
       if ($(window).scrollTop() > 80) {
         $(".lab_logo_text").css("visibility", "hidden");
+        // $(".phone_nav_toggle").css("top", "12px");
       } else {
         $(".lab_logo_text").css("visibility", "visible");
+        // $(".phone_nav_toggle").css("top", "36px");
       }
     }
   });
@@ -36,7 +38,7 @@ $('document').ready(function () {
       $(".lab_logo_text").css("visibility", "visible");
       $(".navbar").css("width", "100%");
       $(".navbar").css("margin-left", "0px");
-      $(".logo_placeholder").attr("onclick", "return true")
+      $(".logo_placeholder").attr("onclick", "return true");
       // $(".overlay").css("visibility", "hidden")
       if ($(window).scrollTop() > 80) {
         $(".navbar").css("padding", "10px 10px");
@@ -50,12 +52,12 @@ $('document').ready(function () {
         $(".lab_logo_text").css("margin-top", "20px");
       }
     } else {
-      $(".slideshow_container").css("top", "88px");
+      $(".slideshow_container").css("top", "90px");
       $(".navbar").css("width", "0px");
       $(".homepage").css("margin-left", "0px");
       $(".lab_logo").css("margin-left", "0px");
       $(".navbar").css("padding", "0px");
-      $(".logo_placeholder").attr("onclick", "return false")
+      $(".logo_placeholder").attr("onclick", "return false");
       // $(".overlay").css("visibility", "visible")
       if ($(window).scrollTop() > 80) {
         $(".lab_logo_text").css("visibility", "hidden");
@@ -98,7 +100,7 @@ $('document').ready(function () {
       $(".lab_logo_text").css("margin-top", "5px");
     }
   } else {
-    $(".slideshow_container").css("top", "88px");
+    $(".slideshow_container").css("top", "90px");
     $(".navbar").css("width", "0px");
     $(".homepage").css("margin-left", "0px");
     $(".lab_logo").css("margin-left", "0px");
@@ -106,8 +108,10 @@ $('document').ready(function () {
     $(".logo_placeholder").attr("onclick", "return false")
     // $(".overlay").css("visibility", "visible")
     if ($(window).scrollTop() > 80) {
+      // $(".phone_nav_toggle").css("top", "12px");
       $(".lab_logo_text").css("visibility", "hidden");
     } else {
+      // $(".phone_nav_toggle").css("top", "36px");
       $(".lab_logo_text").css("visibility", "visible");
     }
   }
@@ -125,14 +129,22 @@ function toggleNav() {
       $(".phone_nav_toggle").css("margin-right", "200px");
       // $(".lab_logo").css("margin-left", "200px")
       // $(".lab_logo_text").css("margin-left", "200px")
-      $(".navbar a:first-child").css("margin-left", "0")
+      $(".lab_logo_text").css("left", "10px");
+      $(".lab_logo_text").css("top", "86px");
+      $(".navbar a:first-child").css("margin-left", "0");
+      $(".slideshow_container").css("top", "136px");
+      $(".article").css("padding-top", "106px");
     } else {
       $(".navbar").css("width", "0px");
       $(".homepage").css("margin-right", "0px");
       $(".phone_nav_toggle").css("margin-right", "0px");
       // $(".lab_logo").css("margin-left", "0px");
       // $(".lab_logo_text").css("margin-left", "0px")
-      $(".navbar a:first-child").css("margin-left", "0")
+      $(".navbar a:first-child").css("margin-left", "0");
+      $(".lab_logo_text").css("left", "90px");
+      $(".lab_logo_text").css("top", "20px");
+      $(".slideshow_container").css("top", "90px");
+      $(".article").css("padding-top", "60px");
     }
   }
 }
